feb651058a5f3d94202d23da87b3f887b28abd41
Ahmed@Atef <47149282+Ahmed3atef@users.noreply.github.com>
2024-08-09 21:36:00 +0300

    adding networkchuck to tv in constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,11 @@ export const dialogueData = {
     pc: `This is my PC. I work mostly in Django/Python these days.
         I've made a couple of projects in that language. I also like JavaScript. Anyway regardless of the language, I just like programming.
         Here is my <a href="https://github.com/Ahmed3atef" target="_blank">Github</a>!`,
-    "cs-degree": `This is my university degree in social work. I did not obtain a university degree in computer science from Egyptian universities, but I obtained CS50 certificates from Harvard University, which provides distance education.  check my <a href="https://www.linkedin.com/in/ahmed-atef-23510018a/" target="_blank">LinkedIn</a> account to see all my certificates`,
+    "cs-degree": `This is my university degree in social work. I did not obtain a university degree in computer science from Egyptian universities, but I obtained CS50 certificates from Harvard University, which provides distance education. check my <a href="https://www.linkedin.com/in/ahmed-atef-23510018a/" target="_blank">LinkedIn</a> account to see all my certificates`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. `,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
         <a href="https://www.youtube.com/@Codezilla" target="_blank">Codezilla</a>, <a href="https://www.youtube.com/@PythonArab" target="_blank">Python Arabic Community</a> ,
-        <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a> (sometimes) and <a href="https://www.youtube.com/@ElzeroWebSchool" target="_blank">Elzero Web School</a>!`,
+        <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a> (sometimes),<a href="https://www.youtube.com/@NetworkChuck" target="_blank"></a> and <a href="https://www.youtube.com/@ElzeroWebSchool" target="_blank">Elzero Web School</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1NCykVvaDO0BzShUrev-rVD94CYdFzXVK/view?usp=drive_link" target="_blank">Check it out?</a>
         Contact me at clay674302@gmail.com if you have any interesting job opportunities!`,

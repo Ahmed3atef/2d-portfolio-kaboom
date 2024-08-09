fd7369b3118dffe3a0698e6ad5d57d17abb85f6a
Ahmed@Atef <47149282+Ahmed3atef@users.noreply.github.com>
2024-08-09 12:31:13 +0300

    finishing update constants

--- src/constants.js
+++ src/constants.js
@@ -2,22 +2,20 @@ export const scaleFactor = 4;
 
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-        I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-        Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
+    pc: `This is my PC. I work mostly in Django/Python these days.
+        I've made a couple of projects in that language. I also like JavaScript. Anyway regardless of the language, I just like programming.
+        Here is my <a href="https://github.com/Ahmed3atef" target="_blank">Github</a>!`,
+    "cs-degree": `This is my university degree in social work. I did not obtain a university degree in computer science from Egyptian universities, but I obtained CS50 certificates from Harvard University, which provides distance education. This is because I consider the certificate to be just a piece of paper, and the experience and knowledge behind the certificate is What any programmer aims for, and fortunately, it is available on the Internet and at the highest universities in the world`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. `,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
         <a href="https://www.youtube.com/@Codezilla" target="_blank">Codezilla</a>, <a href="https://www.youtube.com/@PythonArab" target="_blank">Python Arabic Community</a> ,
         <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a> (sometimes) and <a href="https://www.youtube.com/@ElzeroWebSchool" target="_blank">Elzero Web School</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-        Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-        Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-        Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1NCykVvaDO0BzShUrev-rVD94CYdFzXVK/view?usp=drive_link" target="_blank">Check it out?</a>
+        Contact me at clay674302@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : I learned this project from Freecodecamp to learn about the different and new technologies for me, but do not expect that I can make games. This is not my specialty,
+        but I have a simple background in it. I also have a simple background in creating games using the love2d framework in the Lua language. It's made with the Kaboom.js library which is a library for making games in JavaScript.
+        Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.project src in <a href="https://github.com/Ahmed3atef/profile_website" target"_blank">profile_website</a>`,
     library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit Ahmed Atef portfolio, just close the tab.`,
 };
